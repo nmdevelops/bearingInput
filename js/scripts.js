e7e5476704dece982124e4640a39089fc98b21e2
Nathan Mayer <nm@gmail.com>
2017-06-21 16:48:17 -0700

    add behavior 6, refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -2,19 +2,23 @@
 // declare data delimiter for .split method
 var period = ".";
 //pull string from input
+//behavior 1
 var string = prompt("QDD.MM.SS");
-//behavior 1 - Parse quadrant code
+//behavior 2 - Parse quadrant code
 var quadrant = string.slice(0,1);
-alert(quadrant);
-//behavior 2 - Parse degrees
-var parsedInput = string.slice(1);
-alert(parsedInput);
-var splitInput = parsedInput.split(period, 3);
-alert(splitInput [0]);
-//behavior 3 - Parse minutes
-alert(splitInput [1]);
-//behavior 4 - Parse seconds
-alert(splitInput [2]);
 
+//behavior 3 - Parse degrees
+var parsedInput = string.slice(1);
+
+var splitInput = parsedInput.split(period, 3);
+var degrees = splitInput [0];
+//behavior 4 - Parse minutes
+var minutes = splitInput [1];
+//behavior 5 - Parse seconds
+var seconds = splitInput [2];
+// behavior 6 - display formatted bearing
+var quadrantCardinals = ["Error", "N " + degrees + "° " + minutes + "' " + seconds + '"' + " W", "N " + degrees + "° " + minutes + "' " + seconds + '"' + " E", "S " + degrees + "° " + minutes + "' " + seconds + '"' + " E", "S " + degrees + "° " + minutes + "' " + seconds + '"' + " W"];
+alert(quadrantCardinals [quadrant]);
 
 //front-end logic
+// var quadrantCardinals = ["Error", "North" + bearing + "°" + minutes + "'" + seconds + "''" + "West", "NE", "SE", "SW"]
